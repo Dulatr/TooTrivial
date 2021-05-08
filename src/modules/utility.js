4547e25880bb01752d29a4877743380747dfce2a
Dulatr <echo.dulatr@gmail.com>
2021-05-07 18:16:40 -0600

    Fixed shuffle return on first iteration
    * Moved my decrement to end of loop
    
    * Removed return in while loop

--- src/modules/utility.js
+++ src/modules/utility.js
@@ -1,29 +1,29 @@
 // A module for holding utility functions
 
 //knuth shuffle
-function shuffle(x = []){
+function shuffle(x = []) {
 
-    if (!x){
+    if (!x) {
         return;
     }
 
     var currentPosition = x.length-1;
     var temp, nextPostion;
-    while(currentPosition != 0){
+    while(currentPosition !== 0) {
         // Pick a random position
         nextPostion = Math.floor(Math.random() * currentPosition);
-        //decrement the current position
-        currentPosition -= 1;
-
+        
         // grab the value at current position
         temp = x[currentPosition];
         
         // swap indexes
         x[currentPosition] = x[nextPostion];
+
         // fill
         x[nextPostion] = temp;
 
-        return x;
+        //decrement the current position
+        currentPosition -= 1;
     }
 
     return x;
